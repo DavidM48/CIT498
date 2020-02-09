575a924bb5890180741e65117926ec3687f9762b
DavidM48 <drm20@pct.edu>
2020-02-09 00:20:15 -0500

    added empty color

--- Vega/Helper.py
+++ Vega/Helper.py
@@ -3,5 +3,5 @@ class helper:
     sideDict = {"West":0,"South":1,"North":2,"East":3,"Top":4,"Bottom":5}
     sideDictRev = {0:"West", 1:"South", 2:"North", 3:"East", 4:"Top", 5:"Bottom"}
 
-    colorDict = {"White":0,"Red":1,"Green":2,"Blue":3,"Orange":4,"Yellow":5}
-    colorDictRev = {0:"White", 1:"Red", 2:"Green", 3:"Blue", 4:"Orange", 5:"Yellow"}
+    colorDict = {"White":0,"Red":1,"Green":2,"Blue":3,"Orange":4,"Yellow":5, "Empty":6}
+    colorDictRev = {0:"White", 1:"Red", 2:"Green", 3:"Blue", 4:"Orange", 5:"Yellow", 6:"Empty"}
