3a9a92f69b02596571ddb37e7fd035742b884fe6
DavidM48 <drm20@pct.edu>
2020-02-09 00:20:36 -0500

    Added clean up to the Rubiks cube, sides that dont show won't have a color

--- Vega/Rubiks_Cube.py
+++ Vega/Rubiks_Cube.py
@@ -12,6 +12,7 @@ class Rubiks():
         self.size = size
         self.defaultCube()
         self.initRubiks()
+        self.fillRubiks()
     
     def defaultCube(self):
         self.dc = Cube.Cube(True)
@@ -24,22 +25,47 @@ class Rubiks():
             for y in range(self.size):
                 for z in range(self.size):
                     self.rubiks[x][y][z] = copy.deepcopy(self.dc)
-    
+        self.cleanUp()
+
+    def cleanUp(self):
+        for x in range(self.size):
+            for y in range(self.size):
+                for z in range(self.size):
+                    #remove north or south sides
+                    if y <= 1:
+                        self.rubiks[x][y][z].removeSide(Helper.helper.sideDictRev[1])
+                    if y >= 1:
+                        self.rubiks[x][y][z].removeSide(Helper.helper.sideDictRev[2])
+                    
+                    #remove east or west
+                    if z <= 1:
+                        self.rubiks[x][y][z].removeSide(Helper.helper.sideDictRev[3])
+                    if z >= 1:
+                        self.rubiks[x][y][z].removeSide(Helper.helper.sideDictRev[0])
+
+                    #remove top or bottom
+                    if x <= 1:
+                        self.rubiks[x][y][z].removeSide(Helper.helper.sideDictRev[5])
+                    if x >= 1:
+                        self.rubiks[x][y][z].removeSide(Helper.helper.sideDictRev[4])
+
     def getRubiks(self):
         return self.rubiks
 
+    def test(self):
+        w = ""
+        for x in range(self.size):
+            for y in range(self.size):
+                for z in range(self.size):
+                    w += "[{},{},{}]".format(x,y,z)
+                w += "\t"
+            w += "\n"
+        print(w)
+
 #print(Helper.helper.sideDict)
 rc = Rubiks(3)  
-rc.fillRubiks()
+#rc.fillRubiks()
+rc.test()
 #pprint.pprint(rc.getRubiks())
-print(rc.getRubiks()[0][0][0])
+print(rc.getRubiks()[1][1][0])
 #print(rc.getRubiks()[0][0][0])
-
-#test = Cube.Cube()
-#test.addSide("Top","Red")
-#test.addSide("Bottom","White")
-#test.addSide("Left","Green")
-#test.addSide("Right","Blue")
-#test.addSide("Front","Orange")
-#test.addSide("Back","Yellow")
-#print(test)
