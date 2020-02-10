6a9e8cee2316068bc667495f489a297fef5c73d6
DavidM48 <drm20@pct.edu>
2020-02-10 00:48:35 -0500

    minor changes

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/')
+@app.route('/index.html')
+@app.route('/main.html')
+
 def index():
-        return render_template('index.html')
+        return render_template('main.html', title = 'Vega - Login')
 
 if __name__ == '__main__':
         app.run(debug = True, host = '192.168.3.98')
