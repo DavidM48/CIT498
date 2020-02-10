15d821424d1e0bf9665eac84e1ca783499a95089
DavidM48 <drm20@pct.edu>
2020-02-10 00:42:16 -0500

    Added Hex color dictionary

--- Vega/Helper.py
+++ Vega/Helper.py
@@ -5,3 +5,5 @@ class helper:
 
     colorDict = {"White":0,"Red":1,"Green":2,"Blue":3,"Orange":4,"Yellow":5, "Empty":6}
     colorDictRev = {0:"White", 1:"Red", 2:"Green", 3:"Blue", 4:"Orange", 5:"Yellow", 6:"Empty"}
+    
+    colorDictHex = {"White":0xffffff,"Red":0xff0000,"Green":0x00ff00,"Blue":0x0000ff,"Orange":0xffae00,"Yellow":0xffff00, "Empty":0x000000}
