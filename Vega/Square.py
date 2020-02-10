f57c91f5cc53a5ef7a755731bf756f440f2e8900
DavidM48 <drm20@pct.edu>
2020-02-10 00:44:48 -0500

    Removed storing Strings, now store side/color as an interger

--- Vega/Square.py
+++ Vega/Square.py
@@ -13,15 +13,15 @@ class Square:
 
     def setSide(self, side):
         if type(side) is int:
-            self.side =  Helper.helper.sideDictRev[side]
-        else:
             self.side = side
+        else:
+            self.side = Helper.helper.sideDict[side]
 
     def setColor(self, color):
         if type(color) is int:
-            self.color = Helper.helper.colorDictRev[color]
+            self.color = Helper.helper.colorDictHex[Helper.helper.colorDictRev[color]]
         else:
-            self.color = color
+            self.color = Helper.helper.colorDictRev[color]
 
     def getSide(self):
         return self.side
@@ -30,4 +30,4 @@ class Square:
         return self.color
     
     def __str__(self):
-        return "[Side: {} Color: {}]".format(self.side, self.color) 
+        return "[Side: {}, Color: {}]".format(Helper.helper.sideDictRev[self.side], hex(self.color)) 
