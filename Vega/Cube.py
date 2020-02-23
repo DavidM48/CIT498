bb32504ea030a35d14528600ce3a08f6c235149e
Davidm48 <drm20@pct.edu>
2020-02-23 22:41:38 +0000

    Added swap queue methods

--- Vega/Cube.py
+++ Vega/Cube.py
@@ -12,10 +12,45 @@ class Cube():
             Square.Square(3, 3), 
             Square.Square(4, 4), 
             Square.Square(5, 5)]
+            self.queueReady = False
         else:
             pass
+    
+    def setCords(self, x, y, z):
+        self.x = copy.deepcopy(x)
+        self.y = copy.deepcopy(y)
+        self.z = copy.deepcopy(z)
+    
+    def getCords(self):
+        return [self.x, self.y, self.z]
 
-    def addSide(self, side, color):
+    def swapQueue(self, cube):
+        if type(cube) == Cube:
+            c = copy.deepcopy(cube)
+            self.queue = [c.getSquare(0),
+                    c.getSquare(1),
+                    c.getSquare(2),
+                    c.getSquare(3),
+                    c.getSquare(4),
+                    c.getSquare(5)]
+            self.queueReady = True
+
+    def swapCommitQueue(self):
+        if not self.queueReady:
+            return
+        self.setSquare(0, self.queue[0])
+        self.setSquare(1, self.queue[1])
+        self.setSquare(2, self.queue[2])
+        self.setSquare(3, self.queue[3])
+        self.setSquare(4, self.queue[4])
+        self.setSquare(5, self.queue[5])
+        self.queue = []
+        self.queueReady = False
+
+    def queueState(self):
+        return self.queueReady
+        
+    def setSide(self, side, color):
         self.cube[side] = Square.Square(side, color)
     
     def removeSide(self, side):
@@ -26,6 +61,10 @@ class Cube():
 
     def getSquare(self, side): 
         return self.cube[side]
+    
+    def setSquare(self, side, square: Square):
+        self.cube[side].setColorHex(square.getColor())
+        self.cube[side].setSide(square.getSide())
     
     def createCopy(self):
         return copy.deepcopy(self)
