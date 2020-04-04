5fc2bee5eab263f31d752756dbf02e15deed5976
Davidm48 <drm20@pct.edu>
2020-04-04 20:39:56 +0000

    removed side data

--- Vega/Square.py
+++ Vega/Square.py
@@ -1,30 +1,22 @@
 import Helper
 
 class Square:
-    side = ""
     color = ""
 
     def __init__(self):
         pass
 
-    def __init__(self, side: int, color: int):
-        self.setSide(side)
+    def __init__(self, color: int):
         self.setColor(color)
-
-    def setSide(self, side: int):
-        self.side = side
 
     def setColor(self, color: int):
         self.color = Helper.helper.colorDictHex[Helper.helper.colorDictRev[color]]
 
     def setColorHex(self, color: int):
-        self.color = color
-            
-    def getSide(self):
-        return self.side
+        self.color = color           
     
     def getColor(self):
         return self.color
     
     def __str__(self):
-        return "[Side: {}, Color: {}]".format(Helper.helper.sideDictRev[self.side], hex(self.color)) 
+        return "[Color: {}]".format(hex(self.color)) 
