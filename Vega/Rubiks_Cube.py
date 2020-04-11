ce1b926405c92a8dd18fed14402df8178f959a16
Davidm48 <drm20@pct.edu>
2020-04-11 15:55:22 +0000

    Added Rotate - only works with 3x3x3

--- Vega/Rubiks_Cube.py
+++ Vega/Rubiks_Cube.py
@@ -6,9 +6,7 @@ import pprint
 import copy
 
 class Rubiks():
-    def __init__(self):
-        pass
-
+    
     def __init__(self, size):
         self.size = size
         self.defaultCube()
@@ -28,24 +26,50 @@ class Rubiks():
                     self.rubiks[x][y][z] = copy.deepcopy(self.dc)
                     self.rubiks[x][y][z].setCords(x, y, z)
                     # ! Doesn't work with cubes bigger than 3x3 !
-                    if y <= 1:
-                        self.rubiks[x][y][z].removeSide(1)
-                    if y >= 1:
-                        self.rubiks[x][y][z].removeSide(2)
-                    #remove east or west
-                    if z <= 1:
-                        self.rubiks[x][y][z].removeSide(3)
-                    if z >= 1:
-                        self.rubiks[x][y][z].removeSide(0)
-                    #remove top or bottom
                     if x <= 1:
-                        self.rubiks[x][y][z].removeSide(5)
+                        self.rubiks[x][y][z].removeSide(0)
                     if x >= 1:
+                        self.rubiks[x][y][z].removeSide(1)
+
+                    if y <= 1:
+                        self.rubiks[x][y][z].removeSide(2)
+                    if y >= 1:
+                        self.rubiks[x][y][z].removeSide(3)
+
+                    if z <= 1:
                         self.rubiks[x][y][z].removeSide(4)
+                    if z >= 1:
+                        self.rubiks[x][y][z].removeSide(5)
 
     def getRubiks(self):
         return self.rubiks
+    
+    def getSize(self):
+        return self.size
 
+
+    #pairs - counter clockwise 
+    #0,2 - c
+    #1,5 - s 
+    #2,8 - c
+    #3,1 - s 
+    #4,none
+    #5,7 - s 
+    #6,0 - c
+    #7,3 - s 
+    #8,6 - c
+
+    #pairs - clockwise 
+    #0,6 - c
+    #2,0 - c
+    #6,8 - c
+    #8,2 - c
+
+    #1,3 - s 
+    #3,7 - s 
+    #5,1 - s 
+    #7,5 - s 
+    
     #plain 
     # 0 = vertical = z
     # 1 = horizontal = x
@@ -54,8 +78,7 @@ class Rubiks():
         if (i >= self.size or i < 0):
             #Check to make sure i is within limits
             pass
-        
-        sliced = list()
+        sliced = []
 
         #Fill the list with the cubes we need to swap
         for x in range(self.size):
@@ -66,32 +89,33 @@ class Rubiks():
                     sliced.append((self.rubiks[i][x][y]))
                 else:
                     sliced.append((self.rubiks[x][i][y]))
-        
-        #temp - prints out list of seleted squares
-        for x in range(len(sliced)):
-            print("{}, {}".format(sliced[x].getCords(), sliced[x].getNumberOfSides()))
 
-        #Pair up each cube then add them to a swap queue 
-        #Then commit queue
-        #Need to make sure the sides remain the same as the oringal, ex cube at 0,0,0 should not have a side on the bottom
+        #for x in range(len(sliced)):
+            #print(sliced[x].getCords())
+
         if reverse:
-            for x in range(len(sliced) - 1, -1, -1):
-                pass
+            sliced[0].swapQueue(sliced[6])
+            sliced[1].swapQueue(sliced[3])
+            sliced[2].swapQueue(sliced[0])
+            sliced[3].swapQueue(sliced[7])
+            sliced[5].swapQueue(sliced[1])
+            sliced[6].swapQueue(sliced[8])
+            sliced[7].swapQueue(sliced[5])
+            sliced[8].swapQueue(sliced[2])
         else:
-            for x in range(len(sliced)):
-                for y in range(x + 1, len(sliced)):
-                    if sliced[x].getNumberOfSides() == sliced[y].getNumberOfSides() and not sliced[y].queueState():
-                        sliced[y].swapQueue(sliced[x])
-                        break
-            #might need to fix this for cubes bigger than 3x3
-            sliced[0].swapQueue(sliced[len(sliced) - 1])
-            sliced[1].swapQueue(sliced[len(sliced) - 2])
-        
+            sliced[0].swapQueue(sliced[2])
+            sliced[1].swapQueue(sliced[5])
+            sliced[2].swapQueue(sliced[8])
+            sliced[3].swapQueue(sliced[1])
+            sliced[5].swapQueue(sliced[7])
+            sliced[6].swapQueue(sliced[0])
+            sliced[7].swapQueue(sliced[3])
+            sliced[8].swapQueue(sliced[6])
+
         for x in range(len(sliced)):
-            #print(sliced[x].queueState())
             sliced[x].swapCommitQueue()
-            #sliced[x].rotateCube(plain, reverse)
-        
+            sliced[x].rotateCube(plain, reverse)
+         
     def test(self):
         w = ""
         for x in range(self.size):
