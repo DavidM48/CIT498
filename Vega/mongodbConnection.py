35a24e9247d928326610eff060f19f04d3a9a56a
DavidM48 <46768033+DavidM48@users.noreply.github.com>
2020-10-04 20:37:01 -0400

    Update mongodbConnection.py

--- Vega/mongodbConnection.py
+++ Vega/mongodbConnection.py
@@ -1,7 +1,7 @@
 import pymongo
 import json
 
-client = pymongo.MongoClient('mongodb://cit498:cit498cube@docdb-2020-04-14-19-15-16.cluster-c7jggitb6poy.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false')
+client = pymongo.MongoClient('')
 db = client.moveLog_db
 col = db.movelog
 with open('1588563158658.json') as f:
